16bc6cbe7d30299df19edfc612656c28bb005181
Paul Ledin <paul_ledin@uwalumni.com>
2024-10-22 14:09:09 -0500

    Update SnowFlakeTest.py

--- SnowFlakeTest.py
+++ SnowFlakeTest.py
@@ -12,13 +12,8 @@ import streamlit as st
 # Initialize connection.
 conn = st.connection("snowflake")
 
-# Load the table as a dataframe using the Snowpark Session.
-@st.cache_data
-def load_table():
-    session = conn.session()
-    return session.table("mytable").to_pandas()
-
-df = load_table()
+# Perform query.
+df = conn.query("SELECT * from mytable;", ttl=600)
 
 # Print results.
 for row in df.itertuples():
