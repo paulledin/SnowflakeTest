1453519112fe390b9b8e88f8b1ee9e6f32bab0e7
Paul Ledin <paul_ledin@uwalumni.com>
2024-10-23 17:09:58 -0500

    Update SnowFlakeTest.py

--- SnowFlakeTest.py
+++ SnowFlakeTest.py
@@ -8,13 +8,34 @@ Created on Tue Oct 22 14:01:23 2024
 # streamlit_app.py
 
 import streamlit as st
+import pandas as pd
 
-# Initialize connection.
 conn = st.connection("snowflake")
 
-# Perform query.
-df = conn.query("SELECT * from mytable;", ttl=600)
+def get_report_periods_fromDB():
+    periods = pd.read_csv('https://raw.githubusercontent.com/paulledin/data/master/MonthlyReportPeriods.csv')
+    
+    retVal = list()
+    index = 0
+    for x in periods:
+        retVal.insert(index, periods[x])
+        index += 1
+    
+    return (retVal)
 
-# Print results.
+@st.cache_data
+def load_table():
+    session = conn.session()
+    return session.table("mytable").to_pandas()
+
+df = load_table()
+
 for row in df.itertuples():
     st.write(f"{row.NAME} has a :{row.PET}:")
+
+report_periods = get_report_periods_fromDB()
+st.write(report_periods)
+
+
+
+
