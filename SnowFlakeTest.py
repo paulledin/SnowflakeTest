73d69c0de632d0fa13e74f27593a0623c05c09b4
Paul Ledin <paul_ledin@uwalumni.com>
2024-10-24 16:37:01 -0500

    Update SnowFlakeTest.py

--- SnowFlakeTest.py
+++ SnowFlakeTest.py
@@ -15,15 +15,9 @@ conn = st.connection("snowflake")
 @st.cache_data
 def get_report_periods_fromDB():
     session = conn.session()
-
-    #retVal = session.table("monthly_report.information_schema.tables").to_pandas()
-    retVal = session.sql("SELECT TABLE_NAME, length(TABLE_NAME), substr(TABLE_NAME, 21, 26) FROM monthly_report.information_schema.tables WHERE table_schema = 'BOTH' and TABLE_NAME like 'AFL_TABLE_1_BYSTATE_%' ").to_pandas()
+    retVal = session.sql("SELECT substr(TABLE_NAME, 21, 26) FROM monthly_report.information_schema.tables WHERE table_schema = 'BOTH' and TABLE_NAME like 'AFL_TABLE_1_BYSTATE_%' ").to_pandas()
     
     return retVal
-
-#SELECT TABLE_NAME, length(TABLE_NAME), substr(TABLE_NAME, 21, 26) 
-#FROM monthly_report.information_schema.tables 
-#WHERE table_schema = 'BOTH' and TABLE_NAME like 'AFL_TABLE_1_BYSTATE_%';
 
 report_periods = get_report_periods_fromDB()
 st.write(report_periods)
