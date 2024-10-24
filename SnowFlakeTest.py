43c5e0524a6adf42f97d1e5d747b647e79fa8fe2
Paul Ledin <paul_ledin@uwalumni.com>
2024-10-24 16:10:59 -0500

    Update SnowFlakeTest.py

--- SnowFlakeTest.py
+++ SnowFlakeTest.py
@@ -17,15 +17,9 @@ def get_report_periods_fromDB():
     session = conn.session()
     return session.table("monthly_report.either.afl_table_1_bystate_202409").to_pandas()
 
-@st.cache_data
-def load_table():
-    session = conn.session()
-    return session.table("mytable").to_pandas()
 
-df = load_table()
 
-for row in df.itertuples():
-    st.write(f"{row.NAME} has a :{row.PET}:")
+
 
 report_periods = get_report_periods_fromDB()
 st.write(report_periods)
